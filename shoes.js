89bf095cbcb11ca31bc0e72ab1968923edb56436
Hannah <hannah.hch@gmail.com>
2017-05-01 20:23:36 -0400

    Added Tab Functionality

--- shoes.js
+++ shoes.js
@@ -9,6 +9,22 @@ $(document).ready(function(){
 
   $("#buy").click(function(){
     $("#number").html(Number($("#number").html())+1);
+  });
 
+  $(".tab-content").hide();
+  $("#tab1").show();
+  $("#pd").click(function(){
+    $(".tab-content").hide();
+    $('#tab1').show();
+  });
+
+  $("#size").click(function(){
+    $(".tab-content").hide();
+    $('#tab2').show();
+  });
+
+  $("#review").click(function(){
+    $(".tab-content").hide();
+    $('#tab3').show();
   });
 });
